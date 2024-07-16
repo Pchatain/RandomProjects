a3f1bb33eddec3c474edab4385f6264845dd4a32
pchatain <pchatain@stanford.edu>
2024-07-16 20:14:55 +0200

    wip

--- tree_farmer.lua
+++ tree_farmer.lua
@@ -2,6 +2,18 @@
 local args = {...}
 local FARMLOOPS = tonumber(args[1]) or 1
 
+local SAPPLING_DISTANCE = 2
+local SLEEP_TIME = 5
+
+function forward(n) for i = 1, n do turtle.forward() end end
+
+function back(n) for i = 1, n do turtle.back() end end
+
+function turnaround()
+    assert(turtle.turnRight())
+    assert(turtle.turnRight())
+end
+
 local function refuel()
     if turtle.getFuelLevel() < 20 then
         for i = 1, 16 do
@@ -41,29 +53,17 @@ function fetchBonemealFromChest()
         assert(turtle.down())
         return false
     end
-    assert(turtle.turnRight())
-    assert(turtle.turnRight())
+    turnaround()
     selectEmptySlot()
     got_item = turtle.suck(64)
     assert(turtle.down())
-    assert(turtle.turnRight())
-    assert(turtle.turnRight())
+    turnaround()
     return got_item
-end
-
-local function feedBoneMeal()
-    local boneMealSlot = findBonemeal()
-    if boneMealSlot == 0 then
-        print("No bonemeal found")
-        return fetchBonemealFromChest()
-    end
-    turtle.select(boneMealSlot)
-    turtle.place()
-    return true
 end
 
 local function plantSapling()
     print("Planting sappling")
+    forward(SAPPLING_DISTANCE)
     for i = 1, 16 do
         turtle.select(i)
         local item = turtle.getItemDetail()
@@ -85,15 +85,13 @@ end
 
 local function harvestTree()
     turtle.dig()
-    turtle.forward()
+    forward(1)
     while turtle.detectUp() do
         turtle.digUp()
         turtle.up()
     end
     while not turtle.detectDown() do turtle.down() end
-    collectDrops()
-    turtle.back()
-    collectDrops()
+    back(1)
 end
 
 local function isSapling()
@@ -108,6 +106,50 @@ local function isTree()
     return false
 end
 
+local function feedBoneMealUntilTree()
+    local boneMealSlot = findBonemeal()
+    if boneMealSlot == 0 then
+        print("No bonemeal found")
+        return fetchBonemealFromChest()
+    end
+    turtle.select(boneMealSlot)
+    for i = 1, 10 do
+        turtle.place()
+        if isTree() then return true end
+    end
+    return false
+end
+
+function placeWoodInChest()
+    local woodCount = 0
+    local originalSlot = turtle.getSelectedSlot()
+
+    -- Check if there's a chest in front
+    local success, data = turtle.inspect()
+    if not success or not data.name:find("chest") then
+        print("No chest found in front of the turtle")
+        return 0
+    end
+
+    -- Loop through all inventory slots
+    for slot = 1, 16 do
+        turtle.select(slot)
+        local item = turtle.getItemDetail()
+        if item and (item.name:find("log") or item.name:find("wood")) then
+            -- Found wood, try to place it in the chest
+            local count = turtle.getItemCount()
+            turtle.drop()
+            woodCount = woodCount + count - turtle.getItemCount()
+        end
+    end
+
+    -- Restore original selected slot
+    turtle.select(originalSlot)
+
+    print("Placed " .. woodCount .. " wood items in the chest")
+    return woodCount
+end
+
 function main_loop(farmLoops)
     nTrees = 0
     nLoops = 0
@@ -116,12 +158,23 @@ function main_loop(farmLoops)
         if isTree() then
             harvestTree()
             nTrees = nTrees + 1
+            collectDrops()
+            back(SAPPLING_DISTANCE)
+            collectDrops()
+            turnaround()
+            placeWoodInChest()
+            turnaround()
             if nTrees < farmLoops then plantSapling() end
         elseif isSapling() then
             if nLoops % 30 == 0 then
                 print("Waiting for tree to grow " .. nLoops)
             end
-            os.sleep(2) -- Wait 1 minute
+            if feedBoneMealUntilTree() then
+                print("Tree has grown")
+            else
+                print("Tree didn't grow and no bonemeal, sleeping " .. SLEEP_TIME .. "s")
+                os.sleep(SLEEP_TIME)
+            end
         else
             print("No tree or sapling found. Somting wong")
             break
